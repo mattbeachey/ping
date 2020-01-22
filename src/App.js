386569048ae40413429e3bc45e58b0c65271163c
Matt Beachey <mattbeachey@gmail.com>
2020-01-22 14:28:09 -0600

    Paddles Created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./styles.css";
+import Paddle1 from "./Components/Paddle1";
+import Paddle2 from "./Components/Paddle2";
 
 export default function App() {
   return (
     <div className="App">
+      <Paddle1 />
+      <Paddle2 />
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
---
+++ src/Components/Paddle1.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./paddle.css";
+
+export default function Paddle1() {
+  return <div className="paddle1" />;
+}

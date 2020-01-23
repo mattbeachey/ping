c9788997fee2a91b9ebf156f31c54ef1255856e9
Matt Beachey <mattbeachey@gmail.com>
2020-01-23 13:39:20 -0600

    both paddles move

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 import Paddle from "./Components/Paddle";
 
 
 export default function App() {
+
+  const [p1PaddleY, setP1PaddlyY] = useState(0)
+  const [p2PaddleY, setP2PaddlyY] = useState(0)
+
+//p1 paddle control
+  function handleKey(e) {
+    if (e.key === "s") {
+      if (p1PaddleY < 300) {
+        setP1PaddlyY(p1PaddleY + 25)
+      }
+    }
+    if (e.key === "w") {
+      if (p1PaddleY > -100) {
+        setP1PaddlyY(p1PaddleY - 25)
+      }
+    }
+
+  }
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKey)
+    return () => window.removeEventListener("keydown", handleKey)
+  }, [p1PaddleY])
+
+//p2 paddle control
+  function handleKey2(e) {
+    if (e.key === "o") {
+      if (p2PaddleY < 300) {
+        setP2PaddlyY(p2PaddleY + 25)
+      }
+    }
+    if (e.key === "l") {
+      if (p2PaddleY > -100) {
+        setP2PaddlyY(p2PaddleY - 25)
+      }
+    }
+
+  }
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKey2)
+    return () => window.removeEventListener("keydown", handleKey2)
+  }, [p2PaddleY])
+
   return (
     <div className="gameboard">
-      <Paddle isPlayer1/>
-      <Paddle isPlayer2/>
+      <Paddle paddleY={p1PaddleY} />
+      <Paddle isPlayer2 paddleY={p2PaddleY} />
       <h1>Fight!!!</h1>
     </div>
   );
--- src/Components/Paddle.js
+++ src/Components/Paddle.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./paddle.css";
 
-export default function Paddle({isPlayer2}) {
-  return <div className={isPlayer2? "paddle player2" : "paddle"} />;
+
+export default function Paddle({ isPlayer2, paddleY }) {
+
+  return (
+    <div>
+      <div className={isPlayer2 ? "paddle player2" : "paddle"}
+        style={{ transform: `translateY(calc(10% + ${paddleY}px))` }}
+      />
+    </div>
+  )
 }
